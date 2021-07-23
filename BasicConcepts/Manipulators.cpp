c672cdffbeb502d756f6a5807b32b30e85aa511d
sinchan <sinchanpanda99@gmail.com>
2021-07-24 02:36:18 +0530

    Constants & Manipulators  in C++

--- BasicConcepts/Manipulators.cpp
+++ BasicConcepts/Manipulators.cpp
@@ -6,20 +6,20 @@ using namespace std;
 int main()
 {
 
-    const int a = 3; //--->>Constants in C++
-    cout << "Original value of a : " << a << endl;
-    //int a = 45; //--->>  will throw an error because 'a' is a constant
-    //cout << "Modified value of a is: " << a << endl;
+    const int a = 999; //--->>Constants in C++
+    cout << "Constant value of a : " << a << endl;
+    //int a = 55; //--->>  will throw an error because 'a' is a constant
 
     //Manipulators in C++
+    //Manipulators helps in better formating of data on output screen.
     int x = 10, y = 5232, z = 1999272;
     cout << "\nThe value of a without set width is: " << x << endl;
     cout << "The value of b without set width is: " << y << endl;
     cout << "The value of c without set width is: " << z << endl;
 
-    cout << "\nThe value of a is: " << setw(7) << x << endl;
-    cout << "The value of b is: " << setw(7) << y << endl;
-    cout << "The value of c is: " << setw(7) << z << endl;
+    cout << "\nThe value of a using set width is: " << setw(7) << x << endl;
+    cout << "The value of b using set width is: " << setw(7) << y << endl;
+    cout << "The value of c using set width is: " << setw(7) << z << endl;
 
     return 0;
 }
